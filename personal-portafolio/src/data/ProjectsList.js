31f89b0751ca37807bec556804d1dde3b723b872
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2023-08-24 19:37:20 -0300

    ProjectList

---
+++ personal-portafolio/src/data/ProjectsList.js
@@ -0,0 +1,11 @@
+import Product from "./assets/img/growShop/Product.png"
+import Cart from "./assets/img/growShop/Cart.png"
+import Mobile from "./assets/img/growShop/Mobile.png"
+
+
+export const ProjectList = [
+    {
+        "imgs": [Product,Cart, Mobile],
+        "text": "E-commerce para una pagina web sobre venta de productos para un grow, desarrollada con React y firebase"
+    },
+]
--- personal-portafolio/src/pages/Projects.jsx
+++ personal-portafolio/src/pages/Projects.jsx
@@ -1,7 +1,25 @@
+import { ProjectList } from "../data/ProjectsList"
+
 const Projects = ()=> {
+
+    const getAllProjects = () =>{
+        console.log(ProjectList)
+        return ProjectList.map(Project =>{
+            return(
+                <li className="list-group-item">
+                    <img src={Project.imgs[0]} alt={Project.imgs[0]} className="card-img-top"></img>
+                    <p>{Project.text}</p>
+                </li>
+            )
+        })
+    }
     return(
         <div>
             <h1>Projects</h1>
+            <ul className="list-group">
+                {getAllProjects()}
+            </ul>
+            
         </div>
     )
 }
