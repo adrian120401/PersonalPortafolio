b948e340f2c997c3ef68c0eadbdf8aaa92c75f6d
Adrián de los Reyes <adriandelosreyes2013@gmail.com>
2023-08-31 17:43:37 -0300

    Project cards

--- personal-portafolio/src/data/ProjectsList.js
+++ personal-portafolio/src/data/ProjectsList.js
@@ -6,6 +6,12 @@ import Mobile from "./assets/img/growShop/Mobile.png"
 export const ProjectList = [
     {
         "imgs": [Product,Cart, Mobile],
-        "text": "E-commerce para una pagina web sobre venta de productos para un grow, desarrollada con React y firebase"
+        "title": "E-commerce",
+        "text": <p>E-commerce para una pagina web sobre venta de productos para un grow, desarrollada con React y firebase</p>
+    },
+    {
+        "imgs": [Product,Cart, Mobile],
+        "title": "E-commerce",
+        "text": <p class="card-text">E-commerce para una pagina web sobre venta de productos para un grow, desarrollada con React y firebase</p>
     },
 ]
--- personal-portafolio/src/pages/Projects.jsx
+++ personal-portafolio/src/pages/Projects.jsx
@@ -1,4 +1,5 @@
 import { ProjectList } from "../data/ProjectsList"
+import { Link } from "react-router-dom"
 
 const Projects = ()=> {
 
@@ -6,21 +7,25 @@ const Projects = ()=> {
         console.log(ProjectList)
         return ProjectList.map(Project =>{
             return(
-                <li className="list-group-item">
-                    <img src={Project.imgs[0]} alt={Project.imgs[0]} className="card-img-top"></img>
-                    <p>{Project.text}</p>
-                </li>
+                    <div class="card m-2" style={{backgroundColor:"#323232", color:"white"}} >
+                        <img src={Project.imgs[0]} alt={Project.imgs[0]} className="card-img-top"></img>
+                        <div class="card-body">
+                        <h5 class="card-title">{Project.title}</h5>
+                        {Project.text}
+                        <Link className="btn btn-light">View details</Link>
+                        </div>
+                    </div>
+                
             )
         })
     }
     return(
-        <div>
-            <h1>Projects</h1>
-            <ul className="list-group">
-                {getAllProjects()}
-            </ul>
-            
+        <div class="container-fluid p-4" style={{backgroundColor:"#323232",minHeight:"100vh"}}>
+           <div class="card-group">
+            {getAllProjects()}
+            </div>   
         </div>
+              
     )
 }
 
